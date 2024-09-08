7f2d6ef0a37bce7a1464aacf2e9f2efdbd7cbc44
Bren_ly <bemcamarillo@gmail.com>
2024-09-08 15:14:10 +0800

    Add files via upload

---
+++ TypeCastingDemo.java
@@ -0,0 +1,20 @@
+public class TypeCastingDemo{
+
+	public static void main(String[] args){
+		
+	//variables
+	byte games = 5;
+	int totalGames = games; //widening 
+	
+	int price = 10;
+	double totalPrice = price; 
+	
+		System.out.println("The total games you have is: " + totalGames);
+		System.out.println("The prices of each games is: " + totalPrice);
+		
+
+
+
+
+	}//XI-Acacia Brenly Marion S. Camarillo
+}//end
