9105d860ac5dcc89bd58a21596c0404876dc0e24
Bren_ly <bemcamarillo@gmail.com>
2024-10-23 15:16:52 +0800

    Add files via upload

---
+++ ZooAnimal.java
@@ -0,0 +1,39 @@
+// Jvdroid-main: 
+public class ZooAnimal {
+  private String name;
+  private String type;
+  private int age;
+  private boolean hungry;
+
+  public ZooAnimal() {
+    // Constructor
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public boolean isHungry() {
+    return hungry;
+  }
+}
